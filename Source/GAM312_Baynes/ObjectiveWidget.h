767e7f48d30e3e396afe0e0e0ffe8ee43465093a
Tyler Baynes <tyler.baynes@snhu.edu>
2025-09-20 11:14:18 -0700

    Week 5 complete. Objectives and end game.

---
+++ Source/GAM312_Baynes/ObjectiveWidget.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ObjectiveWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAM312_BAYNES_API UObjectiveWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void UpdatematOBJ(float matsCollected);
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void UpdatebuildOBJ(float objectiveBuilt);
+};
